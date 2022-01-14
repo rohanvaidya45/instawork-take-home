8f6a359e64ec02a2e472819051f31ccd57556836
Rohan Vaidya <rohan.vaidya@berkeley.edu>
2022-01-13 19:16:49 -0800

    validation and grammar fix

--- base/models.py
+++ base/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.core.validators import RegexValidator;
 
 # Create your models here.
 
 class Member(models.Model):
     name = models.CharField(max_length=100)
-    phone = models.CharField(max_length=12)
-    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=12, validators=[RegexValidator(regex=r'^\d{3}-\d{3}-\d{4}$')])
+    email = models.EmailField(max_length=100)
     admin = models.BooleanField(default=False)
 
     def __str__(self):
